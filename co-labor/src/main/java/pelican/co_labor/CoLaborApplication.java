4374a73a6c31f7528e271bc2a02f263d83e29ee4
JoJunHwa <jjj5306@naver.com>
2024-07-04 21:25:28 +0900

    feat : init BE basic project files

---
+++ co-labor/src/main/java/pelican/co_labor/CoLaborApplication.java
@@ -0,0 +1,13 @@
+package pelican.co_labor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoLaborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoLaborApplication.class, args);
+	}
+
+}
