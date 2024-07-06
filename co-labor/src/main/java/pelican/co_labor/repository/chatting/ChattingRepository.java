66160b1e43249db7848c1ac4a37987ef92baa8b6
JoJunHwa <jjj5306@naver.com>
2024-07-06 21:35:29 +0900

    feat : add MainController

--- co-labor/src/main/java/pelican/co_labor/repository/chatting/ChattingRepository.java
+++ co-labor/src/main/java/pelican/co_labor/repository/chatting/ChattingRepository.java
@@ -2,9 +2,9 @@ package pelican.co_labor.repository.chatting;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import pelican.co_labor.domain.enterprise.Enterprise;
+import pelican.co_labor.domain.chatting.Chatting;
 
 
 @Repository
-public interface ChattingRepository extends JpaRepository<Enterprise, Long> {
+public interface ChattingRepository extends JpaRepository<Chatting, Long> {
 }
