eff3a5a67cc95f530a0426c426862080ac7b1425
mk-isos <mkisos7900@naver.com>
2024-07-15 20:45:58 +0900

    Add and modify hospital-related files

---
+++ co-labor/src/main/java/pelican/co_labor/domain/hospital/Hospital.java
@@ -0,0 +1,31 @@
+package pelican.co_labor.domain.hospital;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "hospital")
+public class Hospital {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String dutyAddr;
+
+    @Column(nullable = false)
+    private String dutyName;
+
+    @Column(nullable = false)
+    private String dutyTel1;
+
+    @Column(nullable = false)
+    private double wgs84Lat;
+
+    @Column(nullable = false)
+    private double wgs84Lon;
+}
