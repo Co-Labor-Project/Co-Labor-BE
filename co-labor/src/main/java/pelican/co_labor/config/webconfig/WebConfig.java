58cbdd7cc7608f67f8b94554065f1a32636fdc4d
mk-isos <mkisos7900@naver.com>
2024-07-19 22:33:56 +0900

    refc: 채팅 관련 수정 (토큰, 프롬프트,세션인증, 프론트랑 연결 )

--- co-labor/src/main/java/pelican/co_labor/config/webconfig/WebConfig.java
+++ co-labor/src/main/java/pelican/co_labor/config/webconfig/WebConfig.java
@@ -1,6 +1,9 @@
 package pelican.co_labor.config.webconfig;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +17,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setSameSite("None");
+        serializer.setUseSecureCookie(false); // 로컬 개발 환경에서 false로 설정, 배포 시에는 true로 설정
+        return serializer;
     }
 }
