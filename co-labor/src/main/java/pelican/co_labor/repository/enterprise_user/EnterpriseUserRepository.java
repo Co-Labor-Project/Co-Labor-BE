1fb94f0ef0b5d1709bbb5a960f1a82fba0ab792c
mk-isos <mkisos7900@naver.com>
2024-07-21 17:47:21 +0900

    refc : Add Job Data

--- co-labor/src/main/java/pelican/co_labor/repository/enterprise_user/EnterpriseUserRepository.java
+++ co-labor/src/main/java/pelican/co_labor/repository/enterprise_user/EnterpriseUserRepository.java
@@ -4,13 +4,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import pelican.co_labor.domain.enterprise.Enterprise;
 import pelican.co_labor.domain.enterprise_user.EnterpriseUser;
 
+import java.util.List;
 
 @Repository
 public interface EnterpriseUserRepository extends JpaRepository<EnterpriseUser, String> {
     @Query("SELECT e FROM EnterpriseUser e WHERE e.enterprise_user_id = :enterpriseUserID")
     EnterpriseUser findByEnterpriseUserId(@Param("enterpriseUserID") String enterpriseUserID);
+
+    @Query("SELECT e FROM EnterpriseUser e WHERE e.enterprise = :enterprise")
+    List<EnterpriseUser> findByEnterprise(@Param("enterprise") Enterprise enterprise);
 
     boolean existsByEmail(String email); // 추가된 부분
 }
