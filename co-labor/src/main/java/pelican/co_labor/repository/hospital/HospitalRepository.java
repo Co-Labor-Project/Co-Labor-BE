8a0ae40bbe6b61cd80c7b03320ac6b1a6e9fabeb
mk-isos <mkisos7900@naver.com>
2024-07-30 00:09:50 +0900

    feat: Add import implementation by hospital data region

--- co-labor/src/main/java/pelican/co_labor/repository/hospital/HospitalRepository.java
+++ co-labor/src/main/java/pelican/co_labor/repository/hospital/HospitalRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pelican.co_labor.domain.hospital.Hospital;
 
+import java.util.List;
+
 @Repository
 public interface HospitalRepository extends JpaRepository<Hospital, Long> {
+    List<Hospital> findByDutyAddrStartingWith(String region);
+
+    void deleteByDutyAddrStartingWith(String region);
 }
